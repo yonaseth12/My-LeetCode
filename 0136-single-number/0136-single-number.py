cdf7cd946df3168de04761fc6befacbf099efa3e
Yonas W <88596815+yonaseth12@users.noreply.github.com>
2024-11-15 08:43:38 +0300

    Time: 0 ms (100.00%), Space: 13 MB (96.78%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,11 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        tracker_dict = {}
+        sum = 0
         for i in nums:
-            if i in tracker_dict:
-                del tracker_dict[i]
-            else: tracker_dict[i] = 1
-        key, value = next(iter(tracker_dict.items()))
-        return key
+            sum = sum ^ i
+        return sum
         
