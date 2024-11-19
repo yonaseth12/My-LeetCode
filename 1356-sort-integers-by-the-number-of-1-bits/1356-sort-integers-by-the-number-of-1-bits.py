d135bb5a7fc04f5129ff82f9103beec4417ed418
Yonas W <88596815+yonaseth12@users.noreply.github.com>
2024-11-19 05:49:23 +0300

    Time: 860 ms (5.00%), Space: 11.8 MB (58.93%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def set_bit(self, num):
+        return bin(num).count('1')
+    
+    def sortByBits(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """ 
+        if len(arr) == 1: return arr
+        for i in range(1, len(arr)):
+            if self.set_bit(arr[i]) <= self.set_bit(arr[i-1]):
+                ptr = i
+                if self.set_bit(arr[i]) < self.set_bit(arr[i-1]):
+                    arr[i-1], arr[i] = arr[i], arr[i-1]
+                    ptr = i-1
+                # finish down the road for this element
+                while ptr > 0:
+                    curr_set = self.set_bit(arr[ptr]) 
+                    prev_set = self.set_bit(arr[ptr-1])
+                    if curr_set == prev_set:    # sort in ascending order
+                        if arr[ptr] < arr[ptr-1]:
+                            arr[ptr-1], arr[ptr] = arr[ptr], arr[ptr-1]
+                    elif curr_set < prev_set:
+                        arr[ptr-1], arr[ptr] = arr[ptr], arr[ptr-1]
+                    else: break
+                    ptr -= 1
+        return arr
